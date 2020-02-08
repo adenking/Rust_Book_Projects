28b8148c3eae8d8478e1b1644f45708f41948bdc
Aden King <adenking@live.com>
2020-02-09 04:17:23 +1000

    formatting update

--- projects/CtoFdegree/src/main.rs
+++ projects/CtoFdegree/src/main.rs
@@ -5,7 +5,8 @@ fn main() {
 
     let mut input_temp = String::new();
 
-    io::stdin().read_line(&mut input_temp)
+    io::stdin()
+        .read_line(&mut input_temp)
         .expect("Failed to read line.");
 
     let input_temp: u32 = match input_temp.trim().parse() {
@@ -17,18 +18,19 @@ fn main() {
 
     let mut temp_type = String::new();
 
-    io::stdin().read_line(&mut temp_type)
+    io::stdin()
+        .read_line(&mut temp_type)
         .expect("Failed to read line.");
 
     let mut temp_type = temp_type.trim();
 
     match temp_type {
         "C" => {
-            let c_input_temp = (((input_temp-32)*5)/9);
+            let c_input_temp = (((input_temp - 32) * 5) / 9);
             println!("{} Celcius", c_input_temp);
-        },
+        }
         "F" => {
-            let f_input_temp = input_temp*9/5+32;
+            let f_input_temp = input_temp * 9 / 5 + 32;
             println!("{} Farenheit", f_input_temp);
         }
         _ => println!("Not valid input"),
